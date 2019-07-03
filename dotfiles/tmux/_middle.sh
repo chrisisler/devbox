79350719526052b48b014842d5b757fa5598447a
Chris Isler <4444cisler4444@gmail.com>
2019-07-03 11:42:43 -0400

    update devbox tmuxline

--- dotfiles/tmux/_middle.sh
+++ dotfiles/tmux/_middle.sh
@@ -2,19 +2,7 @@
 
 set -eu
 
-_windows() {
-  local out="$(tmux list-windows)"
-  local windows="$(printf "$out\n" | wc -l | xargs)"
-  local result=""
-  if [[ $windows -le 2  ]]; then
-    local result="$(printf "$out" | awk '{ print $2 }' | tr '\n' ' ' | xargs)"
-  else
-    local result="$(printf "$out" | awk '{ print $1, $2 }' | tr '\n' ' ' | sed -e "s/: /:/g" | xargs)"
-  fi
-  printf "[$result]"
-}
-
 tmuxlineMiddle() {
-  _windows
+  continue
 }
 tmuxlineMiddle
