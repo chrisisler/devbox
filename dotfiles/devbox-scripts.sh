8a5857729f4cfe893e4b8757ee9acf202ef003a9
Chris I <isl.chr@proton.me>
2026-08-20 10:16:47 -0500

    update tmuxconf

--- dotfiles/devbox-scripts.sh
+++ dotfiles/devbox-scripts.sh
@@ -11,8 +11,11 @@ devbox() {
 
   docker run --interactive --tty --rm \
     -e GH_TOKEN="$(gh auth token)" \
+    -p 3000:3000 \
+    -p 5173:5173 \
     --volume "$workdir:/home/devuser/$workdirName" \
-    --volume "$HOME/.ssh/devbox:/home/devuser/.ssh" \
+    --volume "$HOME/Desktop/devbox:/home/devuser/devbox" \
+    --volume "$HOME/.ssh/devbox:/home/devuser/.ssh:ro" \
     --volume "$HOME/.copilot:/home/devuser/.copilot" \
     --name "agentbox-$branchName" \
       ${repo:?}
